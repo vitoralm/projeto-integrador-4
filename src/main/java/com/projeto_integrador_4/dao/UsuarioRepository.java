a9cdbc67678b66ff82663ff919d3392ac40b1b67
Vitor Almeida <vitoralmeidapires@gmail.com>
2020-11-25 18:39:26 -0300

    adicionando teste do modelo de usuario

--- src/main/java/com/projeto_integrador_4/dao/UsuarioRepository.java
+++ src/main/java/com/projeto_integrador_4/dao/UsuarioRepository.java
@@ -1,9 +1,13 @@
 package com.projeto_integrador_4.dao;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.projeto_integrador_4.models.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-	
+	@Query(value = "select * from usuario where login = ?1", nativeQuery = true)
+	public Optional<Usuario> findByLogin(String login);
 }
--- src/test/java/com/projeto_integrador_4/ProjetoIntegrador4ApplicationTests.java
+++ src/test/java/com/projeto_integrador_4/ProjetoIntegrador4ApplicationTests.java
@@ -1,13 +1,40 @@
 package com.projeto_integrador_4;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.projeto_integrador_4.dao.UsuarioRepository;
+import com.projeto_integrador_4.models.Usuario;
 
 @SpringBootTest
 class ProjetoIntegrador4ApplicationTests {
-
+	
+	@Autowired
+	private UsuarioRepository repository;
+	
 	@Test
-	void contextLoads() {
+	void test() {
+		
+		Usuario test = new Usuario();
+		test.setLogin("test_login");
+		test.setSenha("test_senha");
+		test.setEmail("test@test.com");
+		test.setRenda((float) 9999);
+		test.setTelefone("99999999");
+		repository.save(test);
+		Usuario found = repository.findByLogin(test.getLogin()).get();
+		assertThat(found.getLogin().equals(test.getLogin()));
+		
+		Long id = found.getId();
+		
+		repository.findById(id).map(record -> {
+			repository.deleteById(id);
+			return "ok";
+		}).orElse("ok");
+		
 	}
 
 }
